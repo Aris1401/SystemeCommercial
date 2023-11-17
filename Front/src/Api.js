afcc5ace48c4df19f2d92b59ddabf1fa07cb4a00
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2023-11-17 21:14:43 +0300

    inital changes

---
+++ Front/src/Api.js
@@ -0,0 +1,61 @@
+import axios from 'axios'
+
+const API = axios.create({
+  baseURL: 'http://localhost:8080/Back',
+  responseType: 'json',
+  withCredentials: true
+})
+
+const getRequestConfiguration = (authorization) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+  if (authorization) headers.Authorization = `Bearer ${authorization}`
+  return { headers }
+}
+
+const getRequestFormConfiguration = (authorization) => {
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+    }
+    if (authorization) headers.Authorization = `Bearer ${authorization}`
+    return { headers }
+  }
+
+export const makeRequest = ({
+  url,
+  values,
+  successCallback,
+  failureCallback,
+  requestType = 'POST',
+  authorization = null,
+  isFormData = false
+}) => {
+  const requestConfiguration = isFormData ? getRequestFormConfiguration(authorization) : getRequestConfiguration(authorization)
+  let promise
+
+  switch (requestType) {
+    case 'GET':
+      promise = API.get(url, requestConfiguration)
+      break
+    case 'POST':
+      promise = API.post(url, values, requestConfiguration)
+      break
+    case 'DELETE':
+      promise = API.delete(url, requestConfiguration)
+      break
+    default:
+      return
+  }
+
+  promise
+    .then((response) => {
+      const { data } = response
+      successCallback(data)
+    })
+    .catch((error) => {
+      if (error.response) {
+        failureCallback(error.response.data)
+      }
+    })
+}
