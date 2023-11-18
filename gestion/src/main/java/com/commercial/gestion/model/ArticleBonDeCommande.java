3fceb97535205fa32012d1281af843bad93b0a38
Iantenaina <119032741+Iantenaina@users.noreply.github.com>
2023-11-18 19:16:13 +0100

    resteCrud

--- gestion/src/main/java/com/commercial/gestion/model/ArticleBonDeCommande.java
+++ gestion/src/main/java/com/commercial/gestion/model/ArticleBonDeCommande.java
@@ -5,11 +5,15 @@
  */
 package com.commercial.gestion.model;
 
+import com.commercial.gestion.BDDIante.BDD;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author BEST
  */
-public class ArticleBonDeCommande 
+public class ArticleBonDeCommande extends BDD
 {
     int idArticleBonDeCommande;
     int idArticle;
@@ -65,5 +69,39 @@ public class ArticleBonDeCommande
     public void setIdBonDeCommande(int idBonDeCommande) {
         this.idBonDeCommande = idBonDeCommande;
     }
-    
+ //////////////////////////////////////////////////////////////////
+ public ArticleBonDeCommande getArticleBonDeCommandeById(int idArticleBonDeCommande)
+ {
+     ArticleBonDeCommande a=new ArticleBonDeCommande();
+     String condition="idArticleBonDeCommande="+idArticleBonDeCommande;
+     ArrayList<String[]>allArticleBonDeCammande=a.select(condition);
+     for(int i=0;i< allArticleBonDeCammande.size();i++)
+     {
+         a.setIdArticleBonDeCommande(Integer.parseInt(allArticleBonDeCammande.get(i)[0]));
+         a.setIdArticle(Integer.parseInt(allArticleBonDeCammande.get(i)[1]));
+         a.setQuantite(Double.parseDouble(allArticleBonDeCammande.get(i)[2]));
+         a.setPrixUnitaireHT(Double.parseDouble(allArticleBonDeCammande.get(i)[3]));
+         a.setTVA(Double.parseDouble(allArticleBonDeCammande.get(i)[4]));
+         a.setIdBonDeCommande(Integer.parseInt(allArticleBonDeCammande.get(i)[5]));
+     }
+     return a;
+ }
+//////////////////////////////////////////////////////////////////
+public ArticleBonDeCommande getArticleBonDeCommandeForArticle(int idArticle)
+{
+    ArticleBonDeCommande a=new ArticleBonDeCommande();
+    String condition="idArticle="+idArticle;
+    ArrayList<String[]>allArticleBonDeCammande=a.select(condition);
+    for(int i=0;i< allArticleBonDeCammande.size();i++)
+    {
+        a.setIdArticleBonDeCommande(Integer.parseInt(allArticleBonDeCammande.get(i)[0]));
+        a.setIdArticle(Integer.parseInt(allArticleBonDeCammande.get(i)[1]));
+        a.setQuantite(Double.parseDouble(allArticleBonDeCammande.get(i)[2]));
+        a.setPrixUnitaireHT(Double.parseDouble(allArticleBonDeCammande.get(i)[3]));
+        a.setTVA(Double.parseDouble(allArticleBonDeCammande.get(i)[4]));
+        a.setIdBonDeCommande(Integer.parseInt(allArticleBonDeCammande.get(i)[5]));
+    }
+    return a;
+}
+//////////////////////////////////////////////////////////////////
 }
