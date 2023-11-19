3f287783047fbf1e54eea9b61ef77a8978d0049b
Iantenaina <119032741+Iantenaina@users.noreply.github.com>
2023-11-19 12:31:41 +0100

    suite

--- gestion/src/main/java/com/commercial/gestion/model/ProformaArticleBesoinAchat.java
+++ gestion/src/main/java/com/commercial/gestion/model/ProformaArticleBesoinAchat.java
@@ -1,11 +1,16 @@
 package com.commercial.gestion.model;
 
+import com.commercial.gestion.BDDIante.BDD;
 import jakarta.persistence.*;
+import org.hibernate.id.IntegralDataTypeHolder;
+
+import java.util.ArrayList;
 import java.util.Date;
 
 @Entity
 @Table(name = "ProformaArticleBesoinAchat")
-public class ProformaArticleBesoinAchat {
+public class ProformaArticleBesoinAchat extends BDD
+{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +21,7 @@ public class ProformaArticleBesoinAchat {
 
     private int idArticleBesoinAchat;
 
-    @Column(name = "dateLivraison")
+      @Column(name = "dateLivraison")
     private Date dateLivraison;
 
     // Constructors, getters, and setters
@@ -64,5 +69,6 @@ public class ProformaArticleBesoinAchat {
     public void setDateLivraison(Date dateLivraison) {
         this.dateLivraison = dateLivraison;
     }
+
 }
 
